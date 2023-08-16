5403e1f10ec484e253af6a33e86a359c453f88af
RazaAli313 <136004622+RazaAli313@users.noreply.github.com>
2023-08-16 13:05:35 +0500

    Add files via upload

---
+++ Amnesty_International_clone.js
@@ -0,0 +1,6 @@
+window.alert("Welcome to Amnesty International")
+btn=document.getElementById('donation')
+btn.addEventListener('mouseover',function(){
+    alert("Become a Donor of a Community having more than 10 Million members")
+})
+
